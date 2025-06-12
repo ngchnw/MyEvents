4e6f3a62a158d973ebfe02183fef1d363666ff76
ngchnw <ngchn64@gmail.com>
2025-06-12 09:09:40 +0000

    open_modal

---
+++ user-profile.js
@@ -0,0 +1,19 @@
+var edit_profile_button = document.getElementById("edit_profile_button");
+var change_password_button = document.getElementById("change_password_button");
+var delete_account_button = document.getElementById("delete_account_button");
+var report_error_button = document.getElementById("report_error_button");
+var log_out_button = document.getElementById("log_out_button");
+var overlay = document.getElementById("overlay");
+
+var edit_profile_modal = document.getElementById("edit_profile_modal");
+
+edit_profile_button.addEventListener("click", open_modal);
+
+function open_modal() {
+    overlay.style.display = "none";
+    edit_profile_modal.style.display = "block";
+}
+
+
+// .classList.add()
+// .addEventListener()
